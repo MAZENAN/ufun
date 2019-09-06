c4c5bf569576867b0643be8f2d0a16553f7ee919
王宇帆 <183509578@qq.com>
2019-09-07 07:08:43 +0800

    修改配置

--- config/x.config.php
+++ config/x.config.php
@@ -1,5 +1,5 @@
 <?php
 
 return array(
-    'v1'  => array('appid' =>'wxa79545c3d59a4ded' , 'secret' =>'270a85bf0c325f290c54c401f710fd0b')//waimai
+    'v1'  => array('appid' =>'wxccbed80ce5f27348' , 'secret' =>'37e8835c1e0b0b0e2c415c70262bc0c5')//waimai
 );
